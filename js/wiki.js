251d95602ff49c18214709df5faf9d1891c32aea
Simon St Barth <data5tream@protonmail.com>
2017-07-27 05:26:34 +0200

    Visual improvements

--- js/wiki.js
+++ js/wiki.js
@@ -8,14 +8,31 @@ function searchWiki() {
       crossDomain: true,
       headers: { 'Api-User-Agent': 'Data5tream.Wikisearcher/1.0' },
       success: function( data ) {
+        animateWin();
         rdiv.empty();
         rdiv.append("<h4>Results for: \""+data[0]+"\"</h4>");
         for (var i=0; i < data[1].length; i++) {
-          rdiv.append("<div><a href=\""+data[3][i]+"\"><h5>"+data[1][i]+"</h5></a>"+data[2][i]+"</div>")
+          rdiv.append("<div><a href=\""+data[3][i]+"\"><h5>"+data[1][i]+"</h5></a>"+data[2][i]+"</div><hr>")
         }
       }
     } );
   }
+}
+
+function animateWin() {
+  var con = $("#content");
+  con.animate({
+    "top": "10%"
+  }, 250);
+  var v = $(window).width()*0.6+"px";
+  con.animate({
+    "left": "20%",
+    "width": v
+  }, 250);
+  con.animate({
+    "left": "20%",
+    "width": "60%"
+  }, 0); // Changing directly over % doesn't work
 }
 
 $(function () {
